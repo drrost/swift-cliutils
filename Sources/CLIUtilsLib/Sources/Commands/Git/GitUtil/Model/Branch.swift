4c9e37c6b2b7b4359db63890c51b7e61f06c0112
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2021-07-31 19:05:57 +0300

    Add branch type

--- Sources/CLIUtilsLib/Sources/Commands/Git/GitUtil/Model/Branch.swift
+++ Sources/CLIUtilsLib/Sources/Commands/Git/GitUtil/Model/Branch.swift
@@ -7,13 +7,25 @@
 
 import Foundation
 
+public enum BranchType {
+
+    case local
+    case remote
+}
+
 public class Branch {
 
     public let name: String
     public var isCurrent: Bool
+    public let type: BranchType
 
-    public init(_ name: String, _ isCurrent: Bool = false) {
+    public init(
+        _ name: String,
+        _ isCurrent: Bool = false,
+        _ type: BranchType = .local) {
+
         self.name = name
         self.isCurrent = isCurrent
+        self.type = type
     }
 }
